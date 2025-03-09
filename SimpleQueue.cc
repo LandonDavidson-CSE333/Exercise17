bc23c7eebfc5d96fe5f0b836150fb4ee15bdbfc5
Landon Davidson <accounts@landond.tech>
2025-03-08 17:12:35 -0800

    Add: project files

---
+++ SimpleQueue.cc
@@ -0,0 +1,63 @@
+/*
+ * Copyright ©2025 Hal Perkins.  All rights reserved.  Permission is
+ * hereby granted to students registered for University of Washington
+ * CSE 333 for use solely during Winter Quarter 2025 for purposes of
+ * the course.  No other use, copying, distribution, or modification
+ * is permitted without prior written consent. Copyrights for
+ * third-party components of this work must be honored.  Instructors
+ * interested in reusing these course materials should contact the
+ * author.
+ */
+
+#include "SimpleQueue.h"
+
+SimpleQueue::SimpleQueue() {
+  size = 0;
+  front = nullptr;
+  end = nullptr;
+}
+
+SimpleQueue::~SimpleQueue() {
+  while (front != nullptr) {
+    node *next = front->next;
+    delete front;
+    front = next;
+  }
+}
+
+void SimpleQueue::Enqueue(string item) {
+  node *new_node = new node();
+  new_node->next = nullptr;
+  new_node->item = item;
+  if (end != nullptr) {
+    end->next = new_node;
+  } else  {
+    front = new_node;
+  }
+  end = new_node;
+  size++;
+}
+
+bool SimpleQueue::Dequeue(string *result) {
+  if (size == 0) {
+    return false;
+  }
+  *result = front->item;
+  node *next = front->next;
+  delete front;
+  if (end == front) {
+    end = front = next;
+  } else {
+    front = next;
+  }
+  size--;
+  return true;
+}
+
+int SimpleQueue::Size() const {
+  return size;
+}
+
+bool SimpleQueue::IsEmpty() const {
+  return size == 0;
+}
